b70a1e8b67802c64a576f2ea9e05a785c383e976
jihoon <ljihoon0330@gmail.com>
2020-08-10 14:57:09 +0900

    interface 사용해보기

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var person = {
+    name: "jihoon",
+    age: 25,
+    gender: "male"
+};
+var sayHi = function (person) {
+    return "Hello " + person.name + ", you are " + person.age + ", you are a " + person.gender;
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
